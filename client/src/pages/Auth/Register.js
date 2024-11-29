0b52da7efbe9796caf8b21b16a4cbee357a223fe
Lavina <118447154+Lavinaguglani2002@users.noreply.github.com>
2024-11-29 21:50:49 +0530

    Update Register.js

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,71 +1,3 @@
-// import React, { useState } from 'react';
-// import Layout from '../../components/Layout/Layout';
-// import toast from 'react-hot-toast';
-// import { Link, useNavigate } from 'react-router-dom';
-// import axios from 'axios';
-// import '../../styles/AuthStyles.css';
-
-// const Register = () => {
-//   const [name, setName] = useState("");
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [phone, setPhone] = useState("");
-//   const [address, setAddress] = useState("");
-//   const [answer, setAnswer] = useState("");
-//   const navigate = useNavigate();
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const res = await axios.post("http://localhost:8080/api/v1/auth/register", {
-//         name, email, password, phone, address, answer
-//       });
-//       if (res && res.data.success) {
-//         toast.success(res.data.message);
-//         navigate("/login");
-//       } else {
-//         toast.error(res.data.message);
-//       }
-//     } catch (error) {
-//       console.log(error);
-//       toast.error('Something went wrong');
-//     }
-//   };
-
-//   return (
-//     <Layout title="Register - Ecommerce App">
-//       <div className='form-container'>
-//         <h1 className='text-center mb-4'>Register Page</h1>
-//         <form onSubmit={handleSubmit}>
-//           <div className="mb-3">
-//             <input type="text" value={name} onChange={(e) => setName(e.target.value)} required className="form-control" placeholder='Enter your name' />
-//           </div>
-//           <div className="mb-3">
-//             <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required className="form-control" placeholder='Enter your email' />
-//           </div>
-//           <div className="mb-3">
-//             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required className="form-control" placeholder='Enter your password' />
-//           </div>
-//           <div className="mb-3">
-//             <input type="phone" value={phone} onChange={(e) => setPhone(e.target.value)} required className="form-control" placeholder='Enter your phone no' />
-//           </div>
-//           <div className="mb-3">
-//             <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} required className="form-control" placeholder='Enter your address' />
-//           </div>
-//           <div className="mb-3">
-//             <input type="text" value={answer} onChange={(e) => setAnswer(e.target.value)} required className="form-control" placeholder='What is your favorite sports?' />
-//           </div>
-
-//           <button type="submit" className="btn-primary">REGISTER  </button>
-//           <Link to="/login" className="link-style">Already have an account? Login here</Link>
-//         </form>
-//       </div>
-//     </Layout>
-//   );
-// };
-
-// export default Register;
-
 import React, { useState } from 'react';
 import Layout from '../../components/Layout/Layout';
 import toast from 'react-hot-toast';
@@ -189,3 +121,4 @@ const Register = () => {
 };
 
 export default Register;
+
